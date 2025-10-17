babae2940ec50e978e1165c7bc8cb974b042040d
nivedya-1287 <pnivedya2001@gmail.com>
2025-10-17 22:10:09 +0530

    day4

---
+++ home3.py
@@ -0,0 +1,8 @@
+is_logged_in=True
+is_subscribed=False
+
+user_credits = 100
+max_credits = 200
+min_credits = 50.
+credits_valid=(user_credits>=min_credits and user_credits<=max_credits and !=min_credits
+bonus_eligible
